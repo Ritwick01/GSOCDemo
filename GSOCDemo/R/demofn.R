914ee21e427a3e24dd099111595d5ff2236866cf
Ritwick01 <ritwick20461@iiitd.ac.in>
2022-04-17 12:29:54 +0530

    Adding Functions

---
+++ GSOCDemo/R/demofn.R
@@ -0,0 +1,76 @@
+#' @title
+#' Returns the square of the value passed if numeric value is passed
+#' @author
+#' Ritwick Pal \email{ritwick20461@iiitd.ac.in}
+#'
+#' @param ... numeric value
+#'
+#' @return square of the numeric value
+#' @export
+Function1<-function(Value)
+{
+
+  stopifnot("Input must be numeric" = is.numeric(Value))
+
+  return(Value^2)
+}
+#' @title
+#' Returns the third power of the value passed if numeric value is passed
+#' @author
+#' Ritwick Pal \email{ritwick20461@iiitd.ac.in}
+#'
+#' @param ... numeric value
+#'
+#' @return third power of the numeric value
+#' @export
+Function2<-function(Value)
+{
+  stopifnot("Input must be numeric" = is.numeric(Value))
+
+  return(Value^3)
+}
+#' @title
+#' Returns the fourth power of the value passed if numeric value is passed
+#' @author
+#' Ritwick Pal \email{ritwick20461@iiitd.ac.in}
+#'
+#' @param ... numeric value
+#'
+#' @return fourth power of the numeric value
+#' @export
+Function3<-function(Value)
+{
+  stopifnot("Input must be numeric" = is.numeric(Value))
+
+  return(Value^4)
+}
+#' @title
+#' Returns the fifth power of the value passed if numeric value is passed
+#' @author
+#' Ritwick Pal \email{ritwick20461@iiitd.ac.in}
+#'
+#' @param ... numeric value
+#'
+#' @return fifth power of the numeric value
+#' @export
+Function4<-function(Value)
+{
+  stopifnot("Input must be numeric" = is.numeric(Value))
+
+  return(Value^5)
+}
+#' @title
+#' Returns the sixth power of the value passed if numeric value is passed
+#' @author
+#' Ritwick Pal \email{ritwick20461@iiitd.ac.in}
+#'
+#' @param ... numeric value
+#'
+#' @return sixth power of the numeric value
+#' @export
+Function5<-function(Value)
+{
+  stopifnot("Input must be numeric" = is.numeric(Value))
+
+  return(Value^6)
+}
